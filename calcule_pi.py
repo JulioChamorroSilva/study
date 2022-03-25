e69606f8796e126b47f9fdd6b421b6d144f55e32
Gabriel <gabrielhsc95@gmail.com>
2022-03-25 01:20:03 +0100

    Pequenas melhorias
    Retirei imports desnecessários.
    Retirei whitespaces entre nome da função e pertencesses.
    A variável i não precisa ser declarada, uma vez que você não está usando. Tanto dentro ou fora do loop.
    Eu prefiro escrever explicitamente que um número é float

--- calcule_pi.py
+++ calcule_pi.py
@@ -1,28 +1,22 @@
 # -*- coding: utf-8 -*-
-import scipy
-import matplotlib.pyplot
 import numpy as np
 
-print ("Calculando Pi usando o método Monte Carlo")
+print("Calculando Pi usando o método Monte Carlo")
 # O Método Monte Carlo é um meio de calcular a área de uma função sem precisar integrá-la. 
 # Um meio divertido de aprender o método é calculando pi
-N = int (input("Escolha o número de pontos: "))
+N = int(input("Escolha o número de pontos: "))
 # N será o número de pontos jogados em um quadrado de lado 1
 n = 0
 # n será o número de pontos jogados no quadrado que cairão em um círculo de raio 1
-i = 0
 rng = np.random.default_rng()
 # O rng gera numeros aleatórios entre 0 e 1
-for i in range (N):
+for _ in range(N):
     x_axis = rng.random()
     y_axis = rng.random()
-    if x_axis**2 + y_axis**2 < 1:
+    if x_axis**2 + y_axis**2 =< 1.0:
         n += 1
-        # Essa função calcula os pontos dentro do raio r==1 do círculo e acresce a variável n
-    i += 1
-resultado = 4*n/N
+        # Essa função calcula os pontos dentro do raio r=1 do círculo e acresce a variável n
+resultado = 4.0 * n / N
 # Como os calculos nesse programa ocorrem dentro do quadrante positivo da função, 
 # é necessário multiplicar a razão dos valores por 4 para obter o valor correto de pi
 print(f"O valor de pi é: {resultado}")
-    
-
